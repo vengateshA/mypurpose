c83d1b07345644a64ded36bc3f774182eb1a4070
vengateshA <131533066+vengateshA@users.noreply.github.com>
2023-04-24 23:17:34 -0700

    Add files via upload

--- Loginpage.jsx
+++ Loginpage.jsx
@@ -1,5 +1,4 @@
-
-import axios from 'axios';
+  import axios from 'axios';
 import React from 'react';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
@@ -7,24 +6,26 @@ import { useNavigate } from 'react-router-dom';
 
 function Loginpage(){
   const [values, setvalues] = useState({
-    myusername: '',
-    mypassword: ''
+    email: '',
+    password: ''
   })
 
-  
 const navigate = useNavigate()
-
-
     axios.defaults.withCredentials = true;
-
-
-
+    
     const handleSubmit= (e) =>{
       e.preventDefault();
-      axios.post("http://localhost:6002/api/login", values)
+      axios.post("http://localhost:6003/api/login", values)
       .then(res =>{
-        if(res.data.Status ==="Success"){
-            navigate('/')
+        if(res.data.Status ==="success"){
+          if(res.data.data[0].user_type === "admin1"){
+            navigate("/Company")
+          }else if(res.data.data[0].user_type === "admin2"){
+            navigate("/admin2")
+          }else{
+            navigate("/admin3")
+          }
+            
             console.log("meassage wil be checked")
         }
 else{
@@ -33,18 +34,18 @@ else{
       })
       .catch(err=> console.log(err));
     }
-    
+
     return (
   <div>
     <form onSubmit={handleSubmit}>
   <label htmlFor="name"> Username</label>
-  <input name='myusername' onChange={e=>setvalues({...values, myusername:e.target.value})} id='name'  type="text" />
+  <input name='myusername' onChange={e=>setvalues({...values, email:e.target.value})} id='name'  type="text" />
   <label htmlFor='pass'>password</label>
-  <input name='mypassword' onChange={e=>setvalues({...values, mypassword:e.target.value})}  type="text"  />
+  <input name='mypassword' onChange={e=>setvalues({...values, password:e.target.value})}  type="text"  />
   <button type='submit' >submit</button>
     </form>
   </div>
-  
+
     )
 };
 export default Loginpage;
