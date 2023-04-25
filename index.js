48a9929c7dfb82a560790de14cdd37d51095e722
vengateshA <131533066+vengateshA@users.noreply.github.com>
2023-04-24 21:59:43 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -9,194 +9,60 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors(
     {
-    origin: ["http://localhost:5173"],
-    methods: ["POST,GET"],
-    credentials: true
-}
+        origin: ["http://localhost:5173"],
+        methods: ["POST,GET"],
+        credentials: true
+    }
 ))
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 const db = mysql.createPool({
     host: "localhost",
-    user:"root",
-    password:"systemzts@2023",
-    database:"paultryform"
+    user: "root",
+    password: "zts@2023",
+    database: "login_page"
 });
 
-db.getConnection((err)=>{
-    if(err){
+db.getConnection((err) => {
+    if (err) {
         console.log(err)
     }
-    else{
+    else {
         console.log("connected")
     }
 })
 
 
-// get method
-
-app.get("/api/get",(req, res)=>{
-    const sqlGet = "SELECT * FROM inputdata";
-    db.query(sqlGet, (error, result)=>{
-        if(error){
-            res.status(500).send({
-                status:"fail",
-                message:  "something went wrong"
-            })
+app.get("/api/get", (req, res) => {
+    const sqlGet = "";
+    db.query("select * from logindata", (error, result) => {
+        if (error) {
+            console.log(error);
+        } else {
+            return res.send(result);
         }
-        else{
-            res.send(result)
-            // res.status(200).send({
-            //     status: "success",
-            //     meassage: "deleted successfully"
-            // })
-        }    
     })
-   })
+})
 
-   app.get("/api/getme",(req, res)=>{
-    const sqlGet = "SELECT username,contact FROM inputdata";
-    db.query(sqlGet, (error, result)=>{
-        if(error){
-            res.status(500).send({
-                status:"fail",
-                message:  "something went wrong"
+app.post("/api/login", (req, res) => {
+    const sqlcheck = "SELECT * FROM user WHERE email = ? AND password = ? ";
+    db.query(sqlcheck, [req.body.email, req.body.password], (err, data) => {
+        if (err) return res.json({ Message: "error" });
+        if (data.length > 0) {
+            const email = data.email;
+            const password = data.password;
+            const token = jwt.sign({ email, password }, "our-jsonwebtoken-secret-key", { expiresIn: "1d" })
+            res.cookie('token', token);
+            return res.json({
+                Status: "success",data
             })
         }
-        else{
-            // res.send(result)
-            res.status(200).send({
-                status: "success",
-                meassage: "deleted successfully"
-            })
-        }    
+        else {
+            return res.json({ Message: "no records found" })
+        }
     })
-   })
-   
-   
-
-   // delete method
-
-app.delete("/api/delete/:id",(req, res)=>{
-    const{id}= req.params;
-    const sqlDel = "DELETE FROM inputdata WHERE id = ?";
-    db.query(sqlDel, id,(error, result)=>{
-        if(error){
-            res.status(500).send({
-                status: "fail",
-                message: "something went wrong"
-            })
-        }
-        else{
-            res.status(200).send({
-                status: "success",
-                meassage: "deleted successfuly"
-            })
-        }
-    }) 
 })
 
-//  post method
-// when id in auto increment that will not give in the post method
-app.post('/api/post',(req,res)=>{
-    const{ username, contact, email, native} = req.body;
-    const sqlPost = "INSERT INTO inputdata (  username, contact, email, native) VALUES ( ?, ?, ?, ?)";
-    db.query(sqlPost,[ username, contact, email, native], (error,result)=>{
-    if(error){
-        res.status(500).send({
-            status: "fail",
-            message: "something went wrong"
-        })
-    }
-        else{
-              res.send(result)
-            // res.status(200).send({
-            //     status: "success",
-            //     message: "succesfull"
-            // })
-        }
-    
-})
-})
-
-// update method
-   app.put('/api/put/:id',(req,res)=>{
-    const{id} = req.params;
-    const{ username, contact, email, native} = req.body;  
-    const sqlPost = "UPDATE inputdata SET username = ?, contact = ?, email = ?, native = ? WHERE id = 5";
-    db.query(sqlPost,[ username, contact, email, native, id], (error,result)=>{
-    if(error){
-        res.status(500).send({
-            status: "fail",
-            message: "something went wrong"
-        })
-    }
-        else{
-            res.status(200).send({
-                status: "success",
-                message: "succesfull"
-            })
-        }
-    
-})
-})
-
-// updated data get in list using where id is located
-
-app.get("/api/update/:id",(req, res)=>{
-    const{id}= req.params;
-    const sqlUpdate = "SELECT * FROM inputdata WHERE id = ?";
-    db.query(sqlUpdate, id, (error, result)=>{
-        if(error){
-            res.status(500).send({
-                status:"fail",
-                message:  "something went wrong"
-            })
-        }
-        else{
-             // res.send(result)
-            res.status(200).send({
-                status: "success",
-                meassage: "deleted successfully"
-            })
-        }    
-    })
-   })
-
-// checking
-   app.post("/api/login",(req,res)=>{
-    const sqlcheck = "SELECT * FROM logindata WHERE myusername = ? AND mypassword = ?  ";
-    db.query(sqlcheck,[req.body.myusername, req.body.mypassword], (err,data)=>{
-    if(err) return res.json({ Message: "error"});
-    if(data.length>0){
-        const name = data[0].name;
-        const token = jwt.sign({name}, "our-jsonwebtoken-secret-key",{expiresIn: "1d"}) 
-        res.cookie('token', token);
-        return res.json({Status:"success"})
-    } 
-    else{
-        return res.json({Message: "no records found"})
-    }
-
-    // if(error){
-    //     res.status(500).send({
-    //         status: "fail",
-    //         message: "something went wrong"
-    //     })
-    // }
-    //     else{
-        
-    //           res.send(result)
-    //         // res.status(200).send({
-    //         //     status: "success",
-    //         //     message: "succesfull"
-    //         // })
-    //     }
-    
-})
-})
-
-                                                                                                                           
-app.listen(6002,()=>{
+app.listen(6003, () => {
     console.log("listening 6002")
-})
+})  
